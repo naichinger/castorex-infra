988b173d954e529730ad462a7fca665a8b34b629
naichinger <aichinger.niklas@gmail.com>
2024-07-04 17:04:50 +0200

    diagram

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# Create VMs with Terraform
+# Create k3s Cluster with Terraform and Ansible
+
+# Infrastructure
+
+Terraform creates the following resources:
+- Hetzner Firewall
+   - Allows Ingress on ports 80, 443, 22
+   - Allows Egress on all ports (Hetzner default)
+- Hetzner Network: 10.0.0.0/16
+- Hetzner Subnetwork: 10.0.0.0/24
+-  One Hetzner Server: Master Node
+-  Hetzner Servers: Worker Nodes
+   - Amount can be changed in terraform.tfvars
+- Cloudflare DNS record
+    - A RECORD for k3s.[domain.com]
+    - CNAME RECORD for *.k3s.[domain.com] -> k3s.[domain.com]
+- Cloudflare Tunnel
+    - This Tunnel has access to the k3s internal service subnet and can access all services in the cluster.
+
+![img](docs/network.png)
 
 # Create Cluster with Ansible
 
+
+# Setup
+
 ## Install dependencies
+
+1. 
 
 ```
 ansible-galaxy install -r galaxy_deps.yml
---
+++ docs/diagram.py
@@ -0,0 +1,35 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.onprem.compute import Server
+from diagrams.generic.network import Firewall
+from diagrams.saas.cdn import Cloudflare
+
+with Diagram("Terraform Architecture Diagram",filename="network", show=False):
+    with Cluster("Hetzner Infrastructure"):
+        with Cluster("Castorex Network 10.0.0.0/16"):
+            with Cluster("Castorex Sub Network 10.0.0.0/24"):
+                worker1Vps = Server("1. Worker Node")
+                masterVps = Server("Master Node")
+                workerNVps = Server("nth. Worker Node")
+
+        fw = Firewall("Hetzner Firewall")
+    with Cluster("Cloudflare Infrastructure"):
+        dnsRecord = Cloudflare("DNS Record")
+        tunnel = Cloudflare("Cloudflare Tunnel")
+
+    tunnel >> Edge(label="Accesses K3s internal service subnet ") >> masterVps
+
+    dnsRecord >> fw
+    fw >> workerNVps
+    
+'''    
+    dns = Route53("dns")
+    web = ECS("service")
+    
+
+    with Cluster("DB Cluster"):
+        db_primary = RDS("primary")
+        db_primary - [RDS("replica1"),
+                     RDS("replica2")]
+
+    dns >> web >> db_primary
+'''
